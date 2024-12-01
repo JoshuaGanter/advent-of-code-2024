b850261ade21c20ddeb2569f244d208654228024
Joshua Ganter <mail@joshuaganter.de>
2024-12-01 21:01:00 +0100

    Solve day 1, first part

---
+++ days/01/day01.ts
@@ -0,0 +1,52 @@
+import { join } from "@std/path";
+import { assertEquals, assert } from "@std/assert";
+
+type LocationIdLists = { leftList: readonly number[], rightList: readonly number[] };
+
+function parseInput(input: string): LocationIdLists {
+    const leftList: number[] = [];
+    const rightList: number[] = [];
+    
+    for (const line of input.split("\n")) {
+        if (line === "") continue;
+        const [ leftIdAsString, rightIdAsString ] = line.split("   ");
+    
+        const leftId = Number(leftIdAsString);
+        const rightId = Number(rightIdAsString);
+    
+        assert(Number.isSafeInteger(leftId));
+        assert(Number.isSafeInteger(rightId));
+    
+        leftList.push(leftId);
+        rightList.push(rightId);
+    }
+
+    return {
+        leftList,
+        rightList
+    };
+}
+
+function distanceBetween(a: number, b: number): number {
+    return Math.abs(a - b);
+}
+
+function solveFirst(params: LocationIdLists): number {
+    const leftListSorted: number[] = params.leftList.toSorted((a, b) => a - b);
+    const rightListSorted: number[] = params.rightList.toSorted((a, b) => a - b);
+
+    assertEquals(leftListSorted.length, rightListSorted.length);
+
+    let sumOfDistances: number = 0;
+    for (let i = 0; i < leftListSorted.length; i++) {
+        sumOfDistances += distanceBetween(leftListSorted[i], rightListSorted[i]);
+    }
+
+    return sumOfDistances;
+}
+
+if (import.meta.main) {
+    const inputText: string = await Deno.readTextFile(join(import.meta.dirname!, "input.txt"));
+    const input: LocationIdLists = parseInput(inputText);
+    console.log(`Result #1: ${ solveFirst(input) }`);
+}
