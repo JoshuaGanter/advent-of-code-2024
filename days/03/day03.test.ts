3761ec82ff28660f1de51aab50fc705f723301a6
Joshua Ganter <mail@joshuaganter.de>
2024-12-03 10:06:56 +0100

    Solve day 3, second part

--- days/03/day03.test.ts
+++ days/03/day03.test.ts
@@ -1,9 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
-import { parseInput, solveFirst } from "./day03.ts";
+import { parseInput, solveFirst, solveSecond } from "./day03.ts";
 
 const EXAMPLE_DATA: string =
     `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
 
+const EXAMPLE_DATA_2: string =
+    `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+
 Deno.test("Day 03, first part example works", () => {
     assertEquals(solveFirst(parseInput(EXAMPLE_DATA)), 161);
+});
+
+Deno.test("Day 03, second part example works", () => {
+    assertEquals(solveSecond(EXAMPLE_DATA_2), 48);
 });
--- days/03/day03.ts
+++ days/03/day03.ts
@@ -16,10 +16,21 @@ export function solveFirst(lines: string[]): number {
     return sum;
 }
 
+export function solveSecond(text: string): number {
+    const doLines = text.split("do()");
+    const doLinesWithoutDonts = doLines.map((doLine) =>
+        doLine.split("don't()", 2)[0]
+    );
+    return solveFirst(doLinesWithoutDonts);
+}
+
 if (import.meta.main) {
     const inputText: string = await Deno.readTextFile(
         join(import.meta.dirname!, "input.txt"),
     );
     const input: string[] = parseInput(inputText);
     console.log(`Result #1: ${solveFirst(input)}`);
+
+    // Pass in the whole input text, as dos and don'ts can spread multiple lines
+    console.log(`Result #2: ${solveSecond(inputText)}`);
 }
