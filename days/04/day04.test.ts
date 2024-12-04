9f79f95df75bad7dc23569d4e0fd31dddae8c6f8
Joshua Ganter <mail@joshuaganter.de>
2024-12-04 18:02:47 +0100

    Solve day 4, second part

--- days/04/day04.test.ts
+++ days/04/day04.test.ts
@@ -1,7 +1,8 @@
 import { assertEquals } from "jsr:@std/assert";
-import { parseInput, solveFirst } from "./day04.ts";
+import { parseInput, solveFirst, solveSecond } from "./day04.ts";
 
-const EXAMPLE_DATA: string = `MMMSXXMASM
+const EXAMPLE_DATA: string = `
+MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -14,4 +15,8 @@ MXMXAXMASX`;
 
 Deno.test("Day 04, first part example works", () => {
     assertEquals(solveFirst(parseInput(EXAMPLE_DATA)), 18);
+});
+
+Deno.test("Day 04, second part example works", () => {
+    assertEquals(solveSecond(parseInput(EXAMPLE_DATA)), 9);
 });
--- days/04/day04.ts
+++ days/04/day04.ts
@@ -52,6 +52,29 @@ const FILTERS: string[][][] = [
     ],
 ];
 
+const FILTERS_2: string[][][] = [
+    [
+        ["M", ".", "S"],
+        [".", "A", "."],
+        ["M", ".", "S"],
+    ],
+    [
+        ["M", ".", "M"],
+        [".", "A", "."],
+        ["S", ".", "S"],
+    ],
+    [
+        ["S", ".", "M"],
+        [".", "A", "."],
+        ["S", ".", "M"],
+    ],
+    [
+        ["S", ".", "S"],
+        [".", "A", "."],
+        ["M", ".", "M"],
+    ],
+];
+
 export function parseInput(input: string): string[][] {
     return input.split("\n").filter((str) => str !== "").map((str) =>
         Array.from(str)
@@ -82,13 +105,16 @@ function filterMatches(
     return true;
 }
 
-export function solveFirst(lines: string[][]): number {
+function matchFiltersOverInput(
+    lines: string[][],
+    filters: string[][][],
+): number {
     let numberOfHits: number = 0;
     const matrixWidth: number = lines[0].length;
     for (let row = 0; row < lines.length; row++) {
         assertEquals(lines[row].length, matrixWidth);
         for (let column = 0; column < matrixWidth; column++) {
-            for (const filter of FILTERS) {
+            for (const filter of filters) {
                 if (filterMatches(lines, filter, row, column)) {
                     numberOfHits++;
                 }
@@ -98,10 +124,19 @@ export function solveFirst(lines: string[][]): number {
     return numberOfHits;
 }
 
+export function solveFirst(lines: string[][]): number {
+    return matchFiltersOverInput(lines, FILTERS);
+}
+
+export function solveSecond(lines: string[][]): number {
+    return matchFiltersOverInput(lines, FILTERS_2);
+}
+
 if (import.meta.main) {
     const inputText: string = await Deno.readTextFile(
         join(import.meta.dirname!, "input.txt"),
     );
     const input: string[][] = parseInput(inputText);
     console.log(`Result #1: ${solveFirst(input)}`);
+    console.log(`Result #2: ${solveSecond(input)}`);
 }
