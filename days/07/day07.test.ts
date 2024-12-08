3e5ec290ab9028d40f7a8ede8774b5fcd5315a0f
Joshua Ganter <mail@joshuaganter.de>
2024-12-08 17:28:17 +0100

    Solve day 7, second part

--- days/07/day07.test.ts
+++ days/07/day07.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert";
-import { parseInput, solveFirst } from "./day07.ts";
+import { parseInput, solveFirst, solveSecond } from "./day07.ts";
 
 const EXAMPLE_DATA: string = `190: 10 19
 3267: 81 40 27
@@ -12,5 +12,9 @@ const EXAMPLE_DATA: string = `190: 10 19
 292: 11 6 16 20`;
 
 Deno.test("Day 07, first part example works", () => {
-    assertEquals(solveFirst(parseInput(EXAMPLE_DATA)), 3749);
+    assertEquals(solveFirst(parseInput(EXAMPLE_DATA)), 3749n);
+});
+
+Deno.test("Day 07, second part example works", () => {
+    assertEquals(solveSecond(parseInput(EXAMPLE_DATA)), 11387n);
 });
--- days/07/day07.ts
+++ days/07/day07.ts
@@ -1,20 +1,20 @@
 import { join } from "@std/path";
 
 interface ParseResult {
-    testValues: number[];
-    operationValues: number[][];
+    testValues: readonly bigint[];
+    operationValues: readonly (readonly bigint[])[];
 }
 
 export function parseInput(
     input: string,
 ): ParseResult {
     const lines: string[] = input.split("\n").filter((row) => row !== "");
-    const testValues: number[] = [];
-    const operants: number[][] = [];
+    const testValues: bigint[] = [];
+    const operants: bigint[][] = [];
     lines.forEach((line) => {
         const [testValueAsString, operantsAsString] = line.split(": ");
-        testValues.push(Number(testValueAsString));
-        operants.push(operantsAsString.split(" ").map((val) => Number(val)));
+        testValues.push(BigInt(testValueAsString));
+        operants.push(operantsAsString.split(" ").map((val) => BigInt(val)));
     });
 
     return {
@@ -24,28 +24,64 @@ export function parseInput(
 }
 
 function calculateValue(
-    current: number,
-    expected: number,
-    numbers: readonly number[],
+    current: bigint,
+    expected: bigint,
+    numbers: readonly bigint[],
 ): boolean {
     if (numbers.length === 0) {
         return current === expected;
     }
-    const nextNumbers: number[] = Array.from(numbers);
-    const nextNumber: number = nextNumbers.shift()!;
+    const nextNumbers: bigint[] = Array.from(numbers);
+    const nextNumber: bigint = nextNumbers.shift()!;
     return calculateValue(current + nextNumber, expected, nextNumbers) ||
         calculateValue(current * nextNumber, expected, nextNumbers);
 }
 
-export function solveFirst(parseResult: ParseResult): number {
-    let sumOfOperableTestValues: number = 0;
+export function solveFirst(parseResult: ParseResult): bigint {
+    let sumOfOperableTestValues: bigint = BigInt(0);
     for (let i = 0; i < parseResult.testValues.length; i++) {
-        const firstNumber: number = parseResult.operationValues[i].shift()!;
+        const operands: bigint[] = Array.from(parseResult.operationValues[i]);
+        const firstNumber: bigint = operands.shift()!;
         if (
             calculateValue(
                 firstNumber,
                 parseResult.testValues[i],
-                parseResult.operationValues[i],
+                operands,
+            )
+        ) sumOfOperableTestValues += parseResult.testValues[i];
+    }
+    return sumOfOperableTestValues;
+}
+
+function calculateValue2(
+    current: bigint,
+    expected: bigint,
+    numbers: readonly bigint[],
+): boolean {
+    if (numbers.length === 0) {
+        return current === expected;
+    }
+    const nextNumbers: bigint[] = Array.from(numbers);
+    const nextNumber: bigint = nextNumbers.shift()!;
+    return calculateValue2(current + nextNumber, expected, nextNumbers) ||
+        calculateValue2(current * nextNumber, expected, nextNumbers) ||
+        calculateValue2(
+            BigInt(`${current.toString()}${nextNumber.toString()}`),
+            expected,
+            nextNumbers,
+        );
+}
+
+export function solveSecond(parseResult: ParseResult): bigint {
+    let sumOfOperableTestValues: bigint = BigInt(0);
+    for (let i = 0; i < parseResult.testValues.length; i++) {
+        const operands: bigint[] = Array.from(parseResult.operationValues[i]);
+        const firstNumber: bigint = operands.shift()!;
+        if (
+            calculateValue2(
+                firstNumber,
+                parseResult.testValues[i],
+                operands,
             )
         ) sumOfOperableTestValues += parseResult.testValues[i];
     }
@@ -58,4 +94,5 @@ if (import.meta.main) {
     );
     const input: ParseResult = parseInput(inputText);
     console.log(`Result #1: ${solveFirst(input)}`);
+    console.log(`Result #2: ${solveSecond(input)}`);
 }
